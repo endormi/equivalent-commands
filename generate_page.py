2824fe9d2077b88c34c98237b3538dbe59051ef5
endormi <39559256+endormi@users.noreply.github.com>
2023-06-11 18:36:36 +0300

    Generate HTML page of README.md

---
+++ generate_page.py
@@ -0,0 +1,52 @@
+import markdown
+import codecs
+import re
+
+
+with codecs.open('README.md', mode='r', encoding='utf-8') as file:
+    readme_content = file.read()
+
+# Replace triple backticks with <pre><code> tags
+html_content = re.sub(r'```(.*?)```', r'<pre><code>\1</code></pre>', readme_content, flags=re.DOTALL)
+
+
+def remove_code_language(html_content, language):
+    pattern = fr'<pre><code>{language}'
+    return re.sub(pattern, r'<pre><code>', html_content, flags=re.DOTALL)
+
+
+# Define the list of languages to remove
+languages = ['batch', 'bash', 'powershell']
+
+
+# Remove the specified language texts from <pre><code> blocks
+for language in languages:
+    html_content = remove_code_language(html_content, language)
+
+
+# Convert Markdown to HTML
+html_content = markdown.markdown(html_content)
+
+# Remove indentation from HTML
+html_content = re.sub(r'\n\s+', '\n', html_content)
+
+
+# Generate the index.html file
+with codecs.open('index.html', mode='w', encoding='utf-8') as file:
+    file.write(f'''<!DOCTYPE html>
+<html>
+<head>
+    <title>Equivalent commands</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div id="content">
+    {html_content}
+</div>
+
+<footer>
+<p>Made by me</p>
+</footer>
+</body>
+</html>''')
